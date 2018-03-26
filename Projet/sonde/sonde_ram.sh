0e9be6902d1fba560d3505dc56a52fe8e103da73
Vincent de Germiny <vdegerminy@gmail.com>
2018-03-26 15:05:04 +0200

    Add files via upload

--- Projet/sonde/sonde_ram.sh
+++ Projet/sonde/sonde_ram.sh
@@ -10,6 +10,6 @@ used='"used":'+str(ram.used)+', '
 available='"available":'+str(ram.available)+', '
 percent='"percent":'+str(ram.percent)+', '
 date=datetime.datetime.now()
-heure='"heure":'+str(date.hour)+":"+str(date.minute)+":"+str(date.second)+', '
-date='"date":'+str(date.month)+"/"+str(date.day)+"/"+str(date.year)
+heure='"heure": "'+str(date.hour)+":"+str(date.minute)+":"+str(date.second)+'", '
+date='"date":"'+str(date.month)+"/"+str(date.day)+"/"+str(date.year)+'"'
 os.system('echo \'{ ' + used + available + percent + heure + date + ' }\'') 
