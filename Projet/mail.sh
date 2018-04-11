9f63e325ba5dafcb2271d17683ea2938f3a6e687
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-11 17:30:03 +0200

    Add files via upload

--- Projet/mail.sh
+++ Projet/mail.sh
@@ -13,29 +13,29 @@ def info(file):
 		info=info+line
 	f.close()
 	return info
-		
-def temp(file):
+
 
 def collecte():
 	Body=""
 	l=os.listdir('./data')
 	for i in l:
-		if i == "alerte_cpu.json":
+		if i == "alerte_cpu.json" and os.path.getsize('./data/'+i)!=0:
 			cpuBody="Alertes cpu:\n"
 			cpuBody=cpuBody+info('./data/'+i)+"\n"
 			Body=Body+cpuBody
-		elif i == "alerte_temp.json":
+		elif i == "alerte_temp.json" and os.path.getsize('./data/'+i)!=0:
 			tempBody="Alertes temp:\n"
-			tempBody=tempBody+temp('./data/'+i)+"\n"
+			tempBody=tempBody+info('./data/'+i)+"\n"
 			Body=Body+tempBody
-		elif i == "alerte_ram.json":
+		elif i == "alerte_ram.json" and os.path.getsize('./data/'+i)!=0:
 			ramBody="Alertes ram:\n"
-			ramBody=ramBody+ram('./data/'+i)+"\n"
+			ramBody=ramBody+info('./data/'+i)+"\n"
 			Body=Body+ramBody
-		elif i== "alerte_disk.json":
+		elif i== "alerte_disk.json" and os.path.getsize('./data/'+i)!=0:
 			diskBody="Alertes disk:\n"
-			diskBody=diskBody+disk('./data/'+i)+"\n"
+			diskBody=diskBody+info('./data/'+i)+"\n"
 			Body=Body+diskBody
+	os.system('rm ./data/alerte_*')
 	return Body
 
 
@@ -47,19 +47,19 @@ fromAddr='testes0974@gmail.com'
 toAddr='delfeilcasanova@gmail.com'
 
 Head="Alertes:\n"
-body=collecte();
-if(body=""):
-	return 0
+Body=collecte();
+if(Body!=""):	
+	body=Head+Body
 
-fromHead = 'From : %s\r\n' % fromAddr
-toHead = 'To: %s\r\n\r\n' % toAddr
-subjectHead='Subject: Alerte mail'
+	fromHead = 'From : %s\r\n' % fromAddr
+	toHead = 'To: %s\r\n\r\n' % toAddr
+	subjectHead='Subject: Alerte mail'
 
-emailMessage= '%s\n%s\n%s\n\n%s' % (fromHead, toHead, subjectHead, body)
+	emailMessage= '%s\n%s\n%s\n\n%s' % (fromHead, toHead, subjectHead, body)
 
-s=smtplib.SMTP(mailServer, mailServerPort)
-s.set_debuglevel(1)
-s.starttls()
-s.login("testes0974@gmail.com", "fh2fe7ee")
-s.sendmail(fromAddr, toAddr, emailMessage)
-s.quit()
+	s=smtplib.SMTP(mailServer, mailServerPort)
+	s.set_debuglevel(1)
+	s.starttls()
+	s.login("testes0974@gmail.com", "fh2fe7ee")
+	s.sendmail(fromAddr, toAddr, emailMessage)
+	s.quit()
