6ca13e9df27cb962259e9b56cd910f5d05f8eb03
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-11 16:59:50 +0200

    Add files via upload

--- Projet/collecteur.sh
+++ Projet/collecteur.sh
@@ -1,4 +1,4 @@
-
+./garbageCollector.sh
 if test $# -eq 0 
 then
 	./sonde/sonde_nb-users.sh >> data/nb-users.json
@@ -7,16 +7,18 @@ then
 	./sonde/sonde_cpu.sh >> data/cpu.json
 	./sonde/sonde_disk.sh >> data/disk.json
 else
-		if test $1 = "nb-users" || test $1 = "ram" || test $1 = "cpu" || test $1 = "disk"
-		then
-			./sonde/sonde_$1.sh >> data/$1.json
+	if test $1 = "nb-users" || test $1 = "ram" || test $1 = "cpu" || test $1 = "disk"
+	then
+		./sonde/sonde_$1.sh >> data/$1.json
+	else
+		if  test $1 = "temp" 
+		then 
+			python ./sonde/sonde_temp.py >> data/temp.json
 		else
-			if  test $1 = "temp" 
-			then 
-				python ./sonde/sonde_temp.py >> data/temp.json
-			else
-				echo 'Erreur'
-			fi	
-		fi
+			echo 'Erreur'
+		fi	
+	fi
 fi
-
+ 
+./detect_crise.sh
+./mail.sh
