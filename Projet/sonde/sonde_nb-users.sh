54df686772e5d949ba436f7f0fca3f3d675f5c12
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-11 15:05:03 +0200

    Add files via upload

--- Projet/sonde/sonde_nb-users.sh
+++ Projet/sonde/sonde_nb-users.sh
@@ -4,4 +4,4 @@ nbUsers=`users | wc -l`
 heure=`date +%H:%M:%S`
 date=`date +%m/%d/%Y`
 host=`hostname`
-echo '{ "nbusers": '$nbUsers', "heure": "'$heure'", "date": "'$date'" "host": "'$host'" }'
+echo '{ "nbusers": '$nbUsers', "heure": "'$heure'", "date": "'$date'", "host": "'$host'" }'
