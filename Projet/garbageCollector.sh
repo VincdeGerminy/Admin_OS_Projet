9d730652687f51b24f81185d35ab0a8d9e7e3932
Delfeil Casanova <delfeilcasanova@gmail.com>
2018-04-13 17:36:23 +0200

    Add files via upload

--- Projet/garbageCollector.sh
+++ Projet/garbageCollector.sh
@@ -7,6 +7,8 @@ import datetime
 date_curr=datetime.datetime.now()
 mdy=str(date_curr.month)+"/"+str(date_curr.day)+"/"+str(date_curr.year)
 mdy_v2="0"+str(date_curr.month)+"/0"+str(date_curr.day)+"/"+str(date_curr.year) # cas ou format:0x/0y/xxxx
+mdy_v3="0"+str(date_curr.month)+"/"+str(date_curr.day)+"/"+str(date_curr.year) # cas ou format:0x/y/xxxx
+mdy_v4=str(date_curr.month)+"/0"+str(date_curr.day)+"/"+str(date_curr.year) # cas ou format:x/0y/xxxx
 l=os.listdir('./data')
 for i in l:
 	f = open('./data/'+i,'r')
@@ -14,7 +16,8 @@ for i in l:
 	for line in f.readlines():
 		x=json.loads(line)
 		date=x["date"]
-		if date == mdy or date == mdy_v2 :
+		
+		if date == mdy or date == mdy_v2 or date == mdy_v3 or date == mdy_v4 :
 			f_new.write(line)			
 	os.system('mv ./data/new_'+i+' ./data/'+i)
 			
