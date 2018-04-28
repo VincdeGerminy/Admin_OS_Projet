2d477c11a9d40631d1675eef722f2e18357b4332
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-28 18:32:20 +0200

    Add files via upload

--- Projet/requetes_v2.sh
+++ Projet/requetes_v2.sh
@@ -2,6 +2,22 @@ if test $# -eq 0
 then
 	if test -e ./data/cpu.json
 	then
-		http \\samba.univ-avignon.fr\uapv1603134\cpu.json < ./data/cpu.json
-	fi	
+		http http://nom_serveur/cpu.json < ./data/cpu.json
+	fi
+	if test -e ./data/ram.json
+	then
+		http http://nom_serveur/ram.json < ./data/ram.json
+	fi
+	if test -e ./data/nb-users.json
+		then
+			http http://nom_serveur/nb-users.json < ./data/nb-users.json
+		fi
+	if test -e ./data/disk.json
+		then
+			http http://nom_serveur/disk.json < ./data/disk.json
+		fi
+	if test -e ./data/temp.json
+		then
+			http http://nom_serveur/temp.json < ./data/temp.json
+		fi	
 fi
--- Projet/web-service_v2.sh
+++ Projet/web-service_v2.sh
@@ -1,1 +1,30 @@
-http example.org/cpu.json > ./data/report.json
+if [ ! -f ./data/cpu.json ] 
+then
+	http http://nom_serveur/cpu.json > ./data/cpu.json
+else
+	http http://nom_serveur/cpu.json >> ./data/cpu.json
+	
+if [ ! -f ./data/disk.json ] 
+then
+	http http://nom_serveur/disk.json > ./data/disk.json
+else	
+	http http://nom_serveur/disk.json >> ./data/disk.json
+	
+if [ ! -f ./data/temp.json ] 
+then
+	http http://nom_serveur/temp.json > ./data/temp.json
+else
+	http http://nom_serveur/temp.json >> ./data/temp.json
+	
+if [ ! -f ./data/nb-users.json ] 
+then
+	http http://nom_serveur/nb-users.json > ./data/nb-users.json
+else
+	http http://nom_serveur/nb-users.json >> ./data/nb-users.json
+	
+if [ ! -f ./data/ram.json ] 
+then
+	http http://nom_serveur/ram.json > ./data/ram.json
+else
+	http http://nom_serveur/ram.json >> ./data/ram.json
+	
