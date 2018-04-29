b3c193cec3377dc298c4cb21d530f99bed4832e7
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-29 11:31:16 +0200

    Add files via upload

---
+++ ProjetV2/Serveur/webService/disk.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	
+<?php
+	print_r($_POST);
+	$data=json_encode($_POST);
+	$file="./data/disk.json";
+	$f=fopen($file, 'a+');
+	fwrite($f, $data."\n");
+	fclose($f);
+?>
---
+++ ProjetV2/Serveur/webService/nb-users.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+	
+<?php
+	print_r($_POST);
+	$data=json_encode($_POST);
+	$file="./data/nb-users.json";
+	$f=fopen($file, 'a+');
+	fwrite($f, $data."\n");
+	fclose($f);
+?>
