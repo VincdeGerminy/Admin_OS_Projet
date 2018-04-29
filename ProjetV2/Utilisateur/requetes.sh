9566195562f81c3b88be5cb375a60429e52987a0
Vincent de Germiny <vdegerminy@gmail.com>
2018-04-29 11:32:03 +0200

    Add files via upload

--- ProjetV2/Utilisateur/requetes.sh
+++ ProjetV2/Utilisateur/requetes.sh
@@ -7,10 +7,10 @@ import requests
 l=os.listdir('./data')
 for i in l:
 	if i == "cpu.json" or "ram.json" or "temp.json" or "disk.json" or "nb-users.json":
-		
-
-testDir={"bob":"bobi"}
-testDir["Bob"]="blip"
-
-req=requests.post("http://localhost/Supervise/cpu.php", data=testDir)
-print(req.text)
+		pos=len(i-4)
+		name=i[0:pos]
+		f = open('./data/'+i,'r')
+		for line in f.readlines():
+			x=json.loads(line)
+			req=requests.post("http://localhost/Supervise/name.php", data=x)	
+		f.close()	
